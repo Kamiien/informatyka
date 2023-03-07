2e35a122919ff300b715b6767b0f63492eaac399
KamienGit <kmlstkls@gmail.com>
2023-03-07 12:30:16 +0100

    1.py

--- 23 03 07/1.py
+++ 23 03 07/1.py
@@ -1,18 +1,13 @@
 #napisać program który wyznaczy w tablicy n elementowej długość najdłuższego podciągu stałego
 t = [2,4,5,5,5,6,6,7,2,6]
-max_g = 1
-max = 1
-poz_g = 0
-poz = 0
+l = 0
+lg = 1
 for i in range(1,len(t)):
-  if t[i] > t[i-1]:
-    max += 1
-    if max > max_g:
-      max_g = max
-      poz_g = poz
+  if t[i] == t[i-1]:
+    l += 1
+    if l > lg:
+      lg = l
   else:
-    max = 1
-    poz = 1
+    l = 1
 
-print("Długość = ", max_g)
-print("Pozycja = ", poz_g)
+print("Długość = ", lg)
